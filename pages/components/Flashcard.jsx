edb1f2a01c52bb55b1f8fd7a37aa9289b56cbee0
LaureanoDev <laureanoperezlindodev@hotmail.com>
2023-02-02 11:41:07 -0300

    icons added

--- pages/components/Flashcard.jsx
+++ pages/components/Flashcard.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { useState } from "react";
+import { AiOutlineClose } from "react-icons/ai";
+import { RiPencilFill } from "react-icons/ri";
 
-const Flashcard = ({title}) => {
+const Flashcard = ({ title }) => {
   const [front, setFront] = useState(true);
 
   const returnCard = () => {
@@ -12,15 +14,36 @@ const Flashcard = ({title}) => {
     <div
       className={
         front
-          ? "h-32 w-72 bg-red-50 rounded-sm cursor-pointer flex items-center justify-center"
-          : "h-32 w-72 bg-green-400 rounded-sm cursor-pointer flex items-center justify-center"
+          ? "h-32 w-72 bg-red-50 rounded-sm cursor-pointer flex flex-col justify-center"
+          : "h-32 w-72 bg-green-400 rounded-sm cursor-pointer flex flex-col justify-center"
       }
       onClick={returnCard}
     >
-      <div className="w-[280px] h-[110px] flex items-center justify-center">
-        <h1 className={front ? "display text-center overflow-hidden" : "hidden"}>{title}</h1>
-        <h1 className={front ? "hidden" : "display"}>Back</h1>
+      <div className="h-[20%] w-72 shadow-lg flex items-center justify-end gap-2">
+        <RiPencilFill />
+        <AiOutlineClose />
       </div>
+      <div className="h-[80%] w-72 flex items-center justify-center">
+        <h1
+          className={front ? "display text-center overflow-hidden" : "hidden"}
+        >
+          {title}
+        </h1>
+        <h1
+          className={front ? "hidden" : "display text-center overflow-hidden"}
+        >
+          Back
+        </h1>
+      </div>
+
+      {/* <div className="w-[280px] h-[110px] flex items-center justify-center">
+        <h1
+          className={front ? "display text-center overflow-hidden" : "hidden"}
+        >
+          {title}
+        </h1>
+        <h1 className={front ? "hidden" : "display"}>Back</h1>
+      </div> */}
     </div>
   );
 };
